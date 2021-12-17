83941437b13007399419ddd1460654c1b6353fe0
dilshan <dilshanrumesh6@gmail.com>
2021-12-17 15:54:47 +0530

    first

---
+++ lib/main.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int index = 2;
+
+  @override
+  Widget build(BuildContext context) {
+    final items = <Widget>[
+      Icon(
+        Icons.home,
+        size: 30,
+      ),
+      Icon(
+        Icons.search,
+        size: 30,
+      ),
+      Icon(
+        Icons.favorite,
+        size: 30,
+      ),
+      Icon(
+        Icons.settings,
+        size: 30,
+      ),
+      Icon(
+        Icons.person,
+        size: 30,
+      ),
+    ];
+    return Scaffold(
+      appBar: AppBar(title: Text('Bottom Navigation Bar')),
+      drawer: Drawer(
+        // Add a ListView to the drawer. This ensures the user can scroll
+        // through the options in the drawer if there isn't enough vertical
+        // space to fit everything.
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text('Drawer Header'),
+            ),
+            ListTile(
+              title: const Text('Item 1'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text('Item 2'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+      // body: Center(
+      //   child: Image(
+      //     image: AssetImage("assets/1.png"),
+      //   ),
+      // ),
+      backgroundColor: Colors.blue,
+      bottomNavigationBar: CurvedNavigationBar(
+        items: items,
+        height: 60,
+        index: index,
+        backgroundColor: Colors.transparent,
+        onTap: (index) => setState(() => this.index = index),
+      ),
+    );
+  }
+}
